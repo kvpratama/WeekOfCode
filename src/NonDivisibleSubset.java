8f4550d2d4048b373125639afff2e4fe7e52e3d3
kvpratama <kvpratama@gmail.com>
2016-05-26 20:26:24 +0800

    Remove unnecessary line

--- src/NonDivisibleSubset.java
+++ src/NonDivisibleSubset.java
@@ -1,11 +1,6 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
-/**
- * Created by nv49c on 24/5/16.
- */
 public class NonDivisibleSubset {
 
     public static void main(String[] args) {
@@ -14,9 +9,6 @@ public class NonDivisibleSubset {
         int k = in.nextInt();
         int a[] = new int[len];
         ArrayList<Integer> subset = new ArrayList<>();
-        ArrayList<Integer> remove = new ArrayList<>();
-        int subs[] = new int[len];
-        int rem[] = new int[len];
         for (int i = 0; i < len; i++) {
             a[i] = in.nextInt();
         }
@@ -26,17 +18,12 @@ public class NonDivisibleSubset {
                 if ((a[i] + a[j]) % k != 0){
                     if (!subset.contains(a[i])){
                         subset.add(a[i]);
-//                        System.out.println("put ai " + a[i]);
                     }
                     if (!subset.contains(a[j])){
                         subset.add(a[j]);
-//                        System.out.println("put ai " + a[j]);
                     }
                 }
             }
-        }
-        for (int i = 0; i < subset.size(); i++) {
-            System.out.print(subset.get(i));
         }
         System.out.println(subset.size());
     }
